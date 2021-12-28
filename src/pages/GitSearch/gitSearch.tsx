85d4b6f4435ad869b2f2996419b4c8092ea62c0e
Tiago Ssa <tiago.silva301@gmail.com>
2021-12-28 17:50:28 +0000

    Form structure

--- src/pages/GitSearch/gitSearch.tsx
+++ src/pages/GitSearch/gitSearch.tsx
@@ -1,17 +1,28 @@
 import ResultCard from 'components/ResultCard/resultCard';
+import { useState } from 'react';
 
 import './styles.css';
 
+type FormData = {
+  gitApi: string;
+};
+
 const GitSearch = () => {
+  const [formData, setFormData] = useState<FormData>({
+    gitApi: '',
+  });
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log("Changed to: " + event.target.value)
-  }
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData({ ...formData, [name]: value });
+  };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log("Clicked!")
-  }
+    console.log(formData);
+  };
 
   return (
     <div className="git-search-container">
@@ -21,8 +32,10 @@ const GitSearch = () => {
           <div className="form-container">
             <input
               type="text"
+              name="gitApi"
               className="search-input"
               placeholder="Enter your Github username"
+              value={formData.gitApi}
               onChange={handleChange}
             />
             <button type="submit" className="btn btn-primary search-button">
@@ -34,11 +47,11 @@ const GitSearch = () => {
 
       <div className="container info-container">
         <div className="container img-container">
-          <img src="https://avatars.githubusercontent.com/u/64556958?v=4" alt="" />
+          <img src="https://avatars.githubusercontent.com/u/64556958?v=4" alt=""/>
         </div>
         <div className="container description-container">
           <h6>Information</h6>
-          <ResultCard title="Profile:" description="https://api.github.com/users/Tssa301" />
+          <ResultCard title="Profile:"description="https://api.github.com/users/Tssa301"/>
           <ResultCard title="Followers:" description="45" />
           <ResultCard title="Location:" description="Lisbon" />
           <ResultCard title="Name:" description="Tiago Ssa" />
